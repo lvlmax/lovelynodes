e6d1ac9e36ef5d7eb11961bc5db1e4667586c6b3
lvlmax <148289869+lvlmax@users.noreply.github.com>
2025-03-08 01:52:06 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 
 // disable default versioning
-version = "0.1.2"
+version = "0.3"
 
 // jvm target
 val JVM = 17 // 1.8 for 8, 11 for 11
